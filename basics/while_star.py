8d43a2ebede9f813100fffeec22ebdd19e107792
ManjotKaur-27 <mkaur6_be24@gmail.com>
2026-02-11 14:37:32 +0530

    while loop ans star patterns

---
+++ basics/while_star.py
@@ -0,0 +1,18 @@
+#Print 1-5 using while loop:
+i = 1
+while i<=5:
+    print(i)
+    i+=1
+#Print default star pattern
+i = 1
+while i<=5:
+    print(i*'*')
+    i+=1
+
+#Print reverse star pattern
+i = 5
+while i >= 1:
+    print(i * '*')
+    i -= 1
+
+
