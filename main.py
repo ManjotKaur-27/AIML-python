7ff7a5719fa45622afd438846b1caf22a47a98a0
ManjotKaur-27 <mkaur6_be24@gmail.com>
2026-02-11 12:35:13 +0530

    Understood type conversions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello World!")
-print("What is your name?")
-name = input("My name is=")
-print("Hello " + name)
+# print("Hello World!")
+# print("What is your name?")
+# name = input("My name is=")
+# print("Hello " + name)
+old_age = 18
+new_age = int(old_age)+2
+print(new_age)
+num = 8
+print(float(num))
